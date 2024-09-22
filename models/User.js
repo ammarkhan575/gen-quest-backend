4c93772073583e6ae8b48bff909533ecf6447b4c
ammar <ammarkhan575@gmail.com>
2024-09-22 16:15:53 +0530

    ADD: User model created

---
+++ models/User.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true 
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  quizzesTaken: [
+    {
+      quizId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Quiz'
+      },
+      score: {
+        type: Number,
+        required: true
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
