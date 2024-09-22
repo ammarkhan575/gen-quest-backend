13efebe85685b1da24e7d0269c9565473c39c5eb
ammar <ammarkhan575@gmail.com>
2024-09-22 16:16:35 +0530

    ADD: signup functionality

---
+++ controllers/authController.js
@@ -0,0 +1,56 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+const createUser = async (req, res) => {
+    const { name, email, password, confirmPassword } = req.body;
+    try {
+        let user = await User.findOne({ email: email });
+        if (user) {
+            return res.status(400).json({
+                success: false,
+                message: 'User already exists'
+            })
+        }
+        if (password != confirmPassword) {
+            return res.status(400).json({
+                success: false,
+                message: 'Password and confirm password does not match'
+            })
+        }
+        user = new User({ name, email, password });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+        console.log(user, 'this is user');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' },
+        );
+
+        return res.status(201).json({
+            token,
+            success: true,
+            message: 'User created Successfully',
+        })
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: error
+        })
+    }
+}
+
+export {
+    createUser
+}
---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { createUser } from '../controllers/authController.js';
+
+const router = express.Router();
+
+
+router
+    .post('/signup', createUser);
+
+
+export default router;
